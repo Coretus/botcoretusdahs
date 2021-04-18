6ae574ee5b4caa23669d4f5adbdb0ed36d7c464e
Marcos <78313438+Coretus@users.noreply.github.com>
2021-04-18 10:32:26 -0500

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+module.exports = {
+  clientID: process.env.clientID,
+  clientSecret: process.env.clientSecret,
+  callbackURL: process.env.callbackURL,
+  scope:["identify", "guilds"],
+  token: process.env.token,
+  prefix: process.env.prefix,
+};
